8a75294c55dedf470b32d3dae14b5b5f4fe36f22
Wang Feng <fengwang85@gmail.com>
2015-11-02 21:49:04 -0500

    modified:   read_plot_m3d_hdf5_data.py 	add new function, plot fft Amplitude in phi of a data

--- read_plot_m3d_hdf5_data.py
+++ read_plot_m3d_hdf5_data.py
@@ -89,6 +89,20 @@ def readhdf5_2d_time_data(filename,time_id,data_id,plane_id):
 	trianlist=trianlist[0:len(trianlist)/plane_No]
 	return (timeframe,nnode_1_plane,R,Z,trianlist,data_name,data_2D)
 	
+def readhdf5_f_time_data(filename,time_id,data_id,plane_id):
+	(plane_No,timeframe,nnode,ndatas,R,Z,trianlist,data_name,data_3D)=readhdf5_3d_time_data(filename,time_id,data_id)	
+	nnode_1_plane=nnode/plane_No
+	i_begin=plane_id*nnode_1_plane
+	i_end=i_begin+nnode_1_plane
+	R=R[i_begin:i_end]
+	Z=Z[i_begin:i_end]
+	data_2D=data_3D.reshape((plane_No,nnode_1_plane))
+	
+	fdata_2D=numpy.fft.fft(numpy.transpose(data_2D))	
+	data_2D=fdata_2D.real[:,1]
+	trianlist=trianlist[0:len(trianlist)/plane_No]
+	return (timeframe,nnode_1_plane,R,Z,trianlist,data_name,data_2D)
+	
 
 def plot_m3d_hdf5_2D(filename,time_id,data_id,plane_id):
 	import matplotlib.pyplot as plt
@@ -107,6 +121,24 @@ def plot_m3d_hdf5_2D(filename,time_id,data_id,plane_id):
 	plt.title(title_str)
 	plt.colorbar()
 	plt.show()
+		
+def plot_f_m3d_hdf5_2D(filename,time_id,data_id,plane_id):
+	import matplotlib.pyplot as plt
+	import matplotlib.tri as triangle
+	from matplotlib.pyplot import figure, axes, plot, xlabel, ylabel, title, \
+		grid, savefig, show
+	(timeframe,nnode_1_plane,R,Z,trianlist,data_name,data_2D)=readhdf5_f_time_data(filename,time_id,data_id,plane_id)
+	#plt.gca().set_aspect('equal')
+	tri=triangle.Triangulation(R,Z)
+	fig = figure(figsize=(6,8))
+	plt.tripcolor(tri,data_2D)
+	plt.xlabel('R')
+	plt.ylabel('Z');
+	title_str=format('%s, t=%f'%(data_name,timeframe))
+	plt.title(title_str)
+	plt.colorbar()
+	plt.show()
+
 		
 
 
